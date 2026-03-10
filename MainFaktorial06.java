b45cb219b5c8d52c23d5a80964935c17f00775e9
Ayla Fazi <nisafazilatunalya@gmail.com>
2026-03-10 08:53:51 +0700

    main

---
+++ MainFaktorial06.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class MainFaktorial06 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Masukkan nilai: ");
+        int nilai = input.nextInt();
+
+        Faktorial06 fk = new Faktorial06();
+        System.out.println("Nilai faktorial " + nilai + " menggunakan BF: " + fk.faktorialBF(nilai));
+        System.out.println("Nilai faktorial " + nilai + " menggunakan DC: " + fk.faktorialDC(nilai));
+    }
+    
+}
