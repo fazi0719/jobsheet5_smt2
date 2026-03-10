454efb3479da8231f23d7e78963e61d739a69db8
Ayla Fazi <nisafazilatunalya@gmail.com>
2026-03-10 08:59:14 +0700

    pertanyaan 1.2

--- Faktorial06.java
+++ Faktorial06.java
@@ -1,8 +1,10 @@
 public class Faktorial06 {
     int faktorialBF(int n) {
         int fakto = 1;
-        for (int i = 1; i <= n; i++) {
-            fakto = fakto * i;
+        int i = 1;
+        while (i <= n) {
+            fakto *= i;
+            i++;
         }
         return fakto;
     }
